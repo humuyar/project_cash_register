9359f53283592ef0f719d34bebd6e742d48b655a
humuyar <humiq6071@gmail.com>
2024-07-06 00:03:25 -0700

    fix the error

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-let price = 3.26;
-let cid = [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100]
-];
+const price = 3.26;
+const cid = [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.1], ['QUARTER', 4.25], ['ONE', 90], ['FIVE', 55], ['TEN', 20], ['TWENTY', 60], ['ONE HUNDRED', 100]];
 
 const displayChangeDue = document.getElementById('change-due');
 const cash = document.getElementById('cash');
