7502f86c8dd4e7c4ecea55e1e95fb5019a0ece7f
humuyar <humiq6071@gmail.com>
2024-07-06 00:11:53 -0700

    fix the error

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ const cashDrawerDisplay = document.getElementById('cash-drawer-display');
 
 const formatResults = (status, change) => {
   displayChangeDue.innerHTML = `<p>Status: ${status}</p>`;
-  change.map( money => (displayChangeDue.innerHTML += `<p>${money[0]}: $${money[1]}</p>`) );};
+  change.map(
+    money =>
+    (displayChangeDue.innerHTML += `<p>${money[0]}: $${money[1]}</p>`) ); };
 //   return;
 
 const checkCashRegister = () => {
@@ -17,20 +19,17 @@ const checkCashRegister = () => {
     alert('Customer does not have enough money to purchase the item');
     cash.value = '';
     return;
-  }
-
-  if (Number(cash.value) === price) {
-    displayChangeDue.innerHTML =
-      '<p>No change due - customer paid with exact cash</p>';
+  } if (Number(cash.value) === price) {
+    displayChangeDue.innerHTML ='<p>No change due - customer paid with exact cash</p>';
     cash.value = '';
     return;
   }
 
-  let changeDue = Number(cash.value) - price;
-  let reversedCid = [...cid].reverse();
-  let denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
-  let result = { status: 'OPEN', change: [] };
-  let totalCID = parseFloat(
+  const changeDue = Number(cash.value) - price;
+  const reversedCid = [...cid].reverse();
+  const denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+  const result = { status: 'OPEN', change: [] };
+  const totalCID = parseFloat(
     cid
       .map(total => total[1])
       .reduce((prev, curr) => prev + curr)
@@ -45,10 +44,10 @@ const checkCashRegister = () => {
     result.status = 'CLOSED';
   }
 
-  for (let i = 0; i <= reversedCid.length; i++) {
+  for (const i = 0; i <= reversedCid.length; i++) {
     if (changeDue >= denominations[i] && changeDue > 0) {
-      let count = 0;
-      let total = reversedCid[i][1];
+      const count = 0;
+      const total = reversedCid[i][1];
       while (total > 0 && changeDue >= denominations[i]) {
         total -= denominations[i];
         changeDue = parseFloat((changeDue -= denominations[i]).toFixed(2));
