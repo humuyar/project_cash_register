40f2a652d8c50bf4afb3a47b76153fe5ce5635be
humuyar <humiq6071@gmail.com>
2024-07-12 04:55:01 -0700

    fix the error

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const price = 3.26;
-const cid = [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.1], ['QUARTER', 4.25], ['ONE', 90], ['FIVE', 55], ['TEN', 20], ['TWENTY', 60], ['ONE HUNDRED', 100]];
+let price = 3.26;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
 
 const displayChangeDue = document.getElementById('change-due');
 const cash = document.getElementById('cash');
@@ -9,27 +19,36 @@ const cashDrawerDisplay = document.getElementById('cash-drawer-display');
 
 const formatResults = (status, change) => {
   displayChangeDue.innerHTML = `<p>Status: ${status}</p>`;
- change.map(
-    money => (displayChangeDue.innerHTML += `<p>${money[0]}: $${money[1]}</p>`) 
-); };
-//   return;
+  change.map(
+    money => (displayChangeDue.innerHTML += `<p>${money[0]}: $${money[1]}</p>`)
+  );
+  return;
+};
 
 const checkCashRegister = () => {
   if (Number(cash.value) < price) {
     alert('Customer does not have enough money to purchase the item');
     cash.value = '';
     return;
-  } if (Number(cash.value) === price) {
-    displayChangeDue.innerHTML = '<p>No change due - customer paid with exact cash</p>';
+  }
+
+  if (Number(cash.value) === price) {
+    displayChangeDue.innerHTML =
+      '<p>No change due - customer paid with exact cash</p>';
     cash.value = '';
     return;
   }
 
-  const changeDue = Number(cash.value) - price;
-  const reversedCid = [...cid].reverse();
-  const denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
-  const result = { status: 'OPEN', change: [] };
-  const totalCID = parseFloat(cid.map(total => (total[1])).reduce((prev, curr) => prev + curr).toFixed(2));
+  let changeDue = Number(cash.value) - price;
+  let reversedCid = [...cid].reverse();
+  let denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+  let result = { status: 'OPEN', change: [] };
+  let totalCID = parseFloat(
+    cid
+      .map(total => total[1])
+      .reduce((prev, curr) => prev + curr)
+      .toFixed(2)
+  );
 
   if (totalCID < changeDue) {
     return (displayChangeDue.innerHTML = '<p>Status: INSUFFICIENT_FUNDS</p>');
@@ -39,10 +58,10 @@ const checkCashRegister = () => {
     result.status = 'CLOSED';
   }
 
-  for (const i = 0; i <= reversedCid.length; i++) {
+  for (let i = 0; i <= reversedCid.length; i++) {
     if (changeDue >= denominations[i] && changeDue > 0) {
-      const count = 0;
-      const total = reversedCid[i][1];
+      let count = 0;
+      let total = reversedCid[i][1];
       while (total > 0 && changeDue >= denominations[i]) {
         total -= denominations[i];
         changeDue = parseFloat((changeDue -= denominations[i]).toFixed(2));
@@ -92,8 +111,8 @@ const updateUI = change => {
   priceScreen.textContent = `Total: $${price}`;
   cashDrawerDisplay.innerHTML = `<p><strong>Change in drawer:</strong></p>
     ${cid
-    .map(money => `<p>${currencyNameMap[money[0]]}: $${money[1]}</p>`)
-    .join('')}  
+      .map(money => `<p>${currencyNameMap[money[0]]}: $${money[1]}</p>`)
+      .join('')}  
   `;
 };
 
